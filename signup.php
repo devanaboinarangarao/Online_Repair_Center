5e748889113ab1075f6447ad375400505f28b799
Rangarao D <rangarao.d@xyzi.ai>
2021-09-27 15:41:02 +0530

    online repair center frontend files

---
+++ signup.php
@@ -0,0 +1,10 @@
+<?php
+$name=$_POST['name'];
+$password=$_POST['password'];
+$address=$_POST['address'];
+$var = rand(0,1000);
+echo $var;
+//$con=mysql_connect("localhost","root","");
+//$query="insert into Customer(name,)"
+
+?>
